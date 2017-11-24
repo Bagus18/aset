e1ce1d1b8ec6c2f2405cc9e508ecf5679e81ecb5
Anonymous <eggoez@yandex.com>
2017-11-24 10:28:59 +0700

    Update Fri, Nov 24, 2017 10:28:59 AM

--- js/hahaha.js
+++ js/hahaha.js
@@ -1,8 +1,17 @@
 $(function() {
 var emoticon =  [
-			{simbol:':-)', image:'<img src="https://cdnjs.cloudflare.com/ajax/libs/emojione/2.1.0/assets/svg/1f60a.svg" class="ha3"/>'}, 
-			{simbol:':D', image:'<img src="https://cdnjs.cloudflare.com/ajax/libs/emojione/2.1.0/assets/svg/1f600.svg" class="ha3"/>'}
-				];	
+			{simbol:':8', image:'\ud83d\ude0e'}, 
+			{simbol:':love', image:'\u2764\uFE0F'}, 
+			{simbol:':hati', image:'\uD83D\uDC94'}, 
+			{simbol:':hehe', image:'\uD83D\uDE03'}, 
+			{simbol:':wek', image:'\uD83D\uDE1B'}, 
+			{simbol:':wekz', image:'\uD83D\uDE1C'}, 
+			{simbol:':huhu', image:'\uD83D\uDE12'}, 
+			{simbol:':huhuk', image:'\uD83D\uDE22'}, 
+			{simbol:':hah', image:'\ud83d\ude20'}, 
+			{simbol:':haah', image:'\ud83d\ude21'}, 
+			{simbol:':hihi', image:'\uD83D\uDE09'}, 
+			{simbol:':D', image:'\uD83D\uDE00'}
     $.each(emoticon, function(i, item) {
         $(".note").emoticonku(emoticon[i].simbol, emoticon[i].image);
     });
